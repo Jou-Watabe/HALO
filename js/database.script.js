0735589aeb9a3a10c71eab5712e04ccb50201859
Jou-Watabe <107636452+Jou-Watabe@users.noreply.github.com>
2024-07-24 15:06:22 +0900

    Update database.script.js

--- js/database.script.js
+++ js/database.script.js
@@ -13,3 +13,7 @@ db_his.version(2).stores({
 db_his.version(3).stores({
   speeches: "++id,timestamp,content"
 });
+
+db_his.version(4).stores({
+    stickies: "++id,generate_time,run_time,code,text,top,left,color,is_run_cell,group_id"
+});
