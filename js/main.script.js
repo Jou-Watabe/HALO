9465bb620af6bc665545560647f261028388a0e7
Jou-Watabe <107636452+Jou-Watabe@users.noreply.github.com>
2024-12-20 17:01:49 +0900

    Update main.script.js

--- js/main.script.js
+++ js/main.script.js
@@ -2,5 +2,6 @@
 const editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
     mode: 'python',
     lineNumbers: true,
+    autoCloseBrackets: true,
     theme: 'default' // 必要に応じてテーマを変更
 });
